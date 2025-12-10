24a59763ee68ce3f5088838d119257a85eebf846
GarethG <ggomersall@gmail.com>
2025-12-10 17:02:14 +0000

    removed unecessary import of React in tests

--- src/components/layout/Header.spec.tsx
+++ src/components/layout/Header.spec.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import { MemoryRouter, Routes, Route } from 'react-router-dom'
 import { describe, it, beforeEach, afterEach, expect } from 'vitest'
